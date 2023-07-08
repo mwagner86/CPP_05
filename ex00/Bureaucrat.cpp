afce73474e168d7238a0cdd7ecf70e223d88d916
mwagner <mwagner@students.42wolfsburg.de>
2023-07-08 12:36:12 +0200

    added braces to if statement

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -5,10 +5,11 @@
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(std::string const & name, int grade) : _name(name) {
-    if (VERBOSE)
-        std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
-                    << "Default Constructor called " << COLOR_YELLOW << name << COLOR_DEFAULT
-                    << " " << COLOR_CYAN << grade  << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
+                  << "Default Constructor called " << COLOR_YELLOW << name << COLOR_DEFAULT
+                  << " " << COLOR_CYAN << grade << COLOR_DEFAULT << std::endl;
+    }
     if (grade > MINGRADE) {
         throw GradeTooLowException();
     }
@@ -19,28 +20,31 @@ Bureaucrat::Bureaucrat(std::string const & name, int grade) : _name(name) {
 }
 
 Bureaucrat::Bureaucrat(Bureaucrat const & src) : _name(src._name + "_copy") {
-    if (VERBOSE)
-        std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
-                    << "Copy Constructor called " << COLOR_YELLOW << src.getName() << COLOR_DEFAULT
-                    << " " << COLOR_CYAN << src.getGrade()  << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
+                  << "Copy Constructor called " << COLOR_YELLOW << src.getName() << COLOR_DEFAULT
+                  << " " << COLOR_CYAN << src.getGrade() << COLOR_DEFAULT << std::endl;
+    }
     *this = src;
 }
 
 Bureaucrat & Bureaucrat::operator=(Bureaucrat const & rhs) {
-    if (VERBOSE)
-        std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
-                    << "Assignment Constructor called " << COLOR_YELLOW << rhs.getName() << COLOR_DEFAULT
-                    << " " << COLOR_CYAN << rhs.getGrade()  << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
+                  << "Assignment Constructor called " << COLOR_YELLOW << rhs.getName() << COLOR_DEFAULT
+                  << " " << COLOR_CYAN << rhs.getGrade() << COLOR_DEFAULT << std::endl;
+    }
     if ( this != &rhs )
         this->_grade = rhs.getGrade();
     return *this;
 }
 
 Bureaucrat::~Bureaucrat() {
-    if (VERBOSE)
-    std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
-                << "Destructor called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
-                << " " << COLOR_CYAN << this->getGrade()  << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_DEFAULT
+                  << "Destructor called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
+                  << " " << COLOR_CYAN << this->getGrade() << COLOR_DEFAULT << std::endl;
+    }
 }
 
 int Bureaucrat::getGrade() const {
@@ -53,10 +57,11 @@ std::string const & Bureaucrat::getName() const {
 
 // CAVE: increment grade == "--"
 void	Bureaucrat::incrementGrade() {
-    if (VERBOSE)
-        std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_ORANGE
-                    << "incrementGrade() function called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
-                    << " " << COLOR_CYAN << this->getGrade() << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_ORANGE
+                  << "incrementGrade() function called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
+                  << " " << COLOR_CYAN << this->getGrade() << COLOR_DEFAULT << std::endl;
+    }
     if (this->_grade == Bureaucrat::MAXGRADE)
         throw GradeTooHighException();
     this->_grade--;
@@ -64,10 +69,11 @@ void	Bureaucrat::incrementGrade() {
 
 // CAVE: decrement grade == "++"
 void	Bureaucrat::decrementGrade() {
-    if (VERBOSE)
-        std::cout   << COLOR_GREEN << "[Bureaucrat] " << COLOR_BLUE
-                    << "decrementGrade() function called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
-                    << " " << COLOR_MAGENTA << this->getGrade() << COLOR_DEFAULT << std::endl;
+    if (VERBOSE) {
+        std::cout << COLOR_GREEN << "[Bureaucrat] " << COLOR_BLUE
+                  << "decrementGrade() function called " << COLOR_YELLOW << this->getName() << COLOR_DEFAULT
+                  << " " << COLOR_MAGENTA << this->getGrade() << COLOR_DEFAULT << std::endl;
+    }
     if (this->_grade == Bureaucrat::MINGRADE)
         throw GradeTooLowException();
     this->_grade++;
